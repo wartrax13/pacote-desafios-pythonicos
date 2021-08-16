6a0b89027b7daa5dc3a537aa6e31d2fd1462ef89
wartrax13 <pedro.moisesandrade@gmail.com>
2021-08-16 14:14:10 -0300

    Primeiro exercício

--- 01_donuts.py
+++ 01_donuts.py
@@ -9,9 +9,13 @@ Exemplo: donuts(5) retorna 'Number of donuts: 5'
 e donuts(23) retorna 'Number of donuts: many'
 """
 
+
 def donuts(count):
     # +++ SUA SOLUÇÃO +++
-    return
+    if count >= 10:
+        return f'Number of donuts: many'
+    else:
+        return f'Number of donuts: {count}'
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
